8ff15f0c77d4b19ed4950a491400438f284f25a0
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-25 07:55:23 -0500

    Added ID generation

--- pebbleApp/pebble.js
+++ pebbleApp/pebble.js
@@ -1,7 +1,11 @@
+var socketId = parseInt(localStorage.getItem('socketId')) || Math.floor(Math.random() * 11);
+simply.title('Your ID number is ' + socketId);
+localStorage.setItem('socketId', socketId);
+
 simply.on('accelTap', function(e) {
-    simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+  simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
 });
 
 simply.on('singleClick', function(e) {
-    simply.subtitle('You pressed the ' + e.button + ' button!');
+  simply.subtitle('You pressed the ' + e.button + ' button!');
 });
