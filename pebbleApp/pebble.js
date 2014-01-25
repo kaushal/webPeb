cad64695119f2c031aaff232e9628b693a52778e
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-25 07:57:00 -0500

    Added comments because I will be braindead soon

--- pebbleApp/pebble.js
+++ pebbleApp/pebble.js
@@ -1,11 +1,14 @@
+//Generate a random ID if one does not exist in local storage already.
 var socketId = parseInt(localStorage.getItem('socketId')) || Math.floor(Math.random() * 11);
 simply.title('Your ID number is ' + socketId);
 localStorage.setItem('socketId', socketId);
 
+//Listen for accelerometer data.
 simply.on('accelTap', function(e) {
   simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
 });
 
+//Listen for button clicks.
 simply.on('singleClick', function(e) {
   simply.subtitle('You pressed the ' + e.button + ' button!');
 });
