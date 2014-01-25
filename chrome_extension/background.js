ec5b4ea23460a7c471583a6a1315de8db4528d24
Eric Song <regonics@gmail.com>
2014-01-25 07:30:15 -0500

    created gitignore, added chrome code to change tabs

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,1 +1,27 @@
 //hi
+console.log("hi");
+
+//chrome.tabs.create({url:"http://www.google.com"});
+
+chrome.tabs.update(60, {active: true});
+chrome.tabs.update(69, {active: false});
+
+/*
+chrome.tabs.query({active: true}, function(tabs){
+	for(var i = 0;  i < tabs.length; i++){
+		chrome.tabs.duplicate(tabs[i].id);
+	}
+});
+*/
+
+chrome.tabs.highlight({tabs: 2}, function(window){
+	console.log(window);	
+});
+
+/*
+chrome.tabs.update(0, {selected: true}, function(){
+	console.log("updated tab");
+});
+
+chrome.tabs.duplicate(1);
+[*/
