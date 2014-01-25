d5f1e1974a2e549d68c5cfb17c14145680d30836
Kaushal Parikh <kaushalp88@gmail.com>
2014-01-25 02:59:35 -0800

    chrome and pebble innitial connections

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -9,8 +9,18 @@ var chromeDict = {}
 
 io.sockets.on('connection', function(socket){
     socket.on('pebCon', function(data){
-        console.log('New pebble connection recieved' + data.toString());
-        pebbleDict[data['number']]
+        console.log('New pebble connection recieved ' + data.toString());
+        pebbleDict[socket.id.toString()] = data['number'];
+    });
+
+    socket.on('chromeCon', function(data){
+        console.log('New chrome connection recieved ' + data.toString());
+        chromeDict[socket.id.toString()] = data['number'];
+    });
+
+    socket.on('pebUpdate', function(data){
+        console.log('New pebble update received ' + data.toString());
+        //send information to the chrome client when this happens
     });
 
 });
