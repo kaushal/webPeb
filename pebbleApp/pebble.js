f71fea9a7f254ea2d2e431c4737897d44277b2e4
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-25 16:15:34 -0500

    Added ajax requests

--- pebbleApp/pebble.js
+++ pebbleApp/pebble.js
@@ -6,9 +6,27 @@ localStorage.setItem('socketId', socketId);
 //Listen for accelerometer data.
 simply.on('accelTap', function(e) {
   simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+  var options = {
+    url: 'http://www.webpeb.com/accelerometer',
+    method: 'post',
+    data: {'direction': e.direction, 'axis': e.axis}
+  };
+
+  ajax(options, function(data){
+    simply.vibe('short');
+  });
 });
 
 //Listen for button clicks.
 simply.on('singleClick', function(e) {
   simply.subtitle('You pressed the ' + e.button + ' button!');
+  var options = {
+    url: 'http://www.webpeb.com/button',
+    method: 'post',
+    data: {'button': e.button}
+  };
+
+  ajax(options, function(data){
+    simply.vibe('short');
+  });
 });
