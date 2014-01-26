a944895bee61783069f385a03617c0154fb2eea7
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-26 00:17:53 -0500

    Interprets data on the watch before requests are made

--- pebbleApp/pebble.js
+++ pebbleApp/pebble.js
@@ -6,15 +6,31 @@ var session = function(socketId){
 
   //Listen for accelerometer data.
   simply.on('accelTap', function(e) {
+    var action;
+    if(e.axis === 'y' || e.axis === 'z'){
+      if(e.direction > 0){
+        action = 'tabLeft';
+      }else{
+        action = 'tabRight';
+      }
+    }else{
+      if(e.direction > 0){
+        action = 'closeTab';
+      }else{
+        action = 'reopenTab';
+      }
+    }
+
     simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
 
     var options = {
       url: 'http://107.170.250.170/updateSession',
       method: 'post',
-      data: {'direction': e.direction, 'axis': e.axis, 'pebID': socketId}
+      data: {'action': action, 'pebID': socketId}
     };
 
     ajax(options, function(data){
+      simply.subtitle(action);
       simply.vibe('short');
     });
   });
@@ -23,13 +39,23 @@ var session = function(socketId){
   simply.on('singleClick', function(e) {
     simply.subtitle('You pressed the ' + e.button + ' button!');
 
+    var action;
+    if(e.button === 'down'){
+      action = 'scrollDown';
+    }else if(e.button === 'up'){
+      action = 'scrollUp';
+    }else{
+      action = 'select';
+    }
+
     var options = {
       url: 'http://107.170.250.170/updateSession',
       method: 'post',
-      data: {'button': e.button, 'pebID': socketId}
+      data: {'action': action, 'pebID': socketId}
     };
 
     ajax(options, function(data){
+      simply.subtitle(action);
       simply.vibe('short');
     });
   });
