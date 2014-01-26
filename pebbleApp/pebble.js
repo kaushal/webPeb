b6f402b99366dfc99b7463aa01c2c72538d96ebf
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-25 21:37:35 -0500

    Restructured code and added server address

--- pebbleApp/pebble.js
+++ pebbleApp/pebble.js
@@ -1,32 +1,36 @@
-//Generate a random ID if one does not exist in local storage already.
-var socketId = parseInt(localStorage.getItem('socketId')) || Math.floor(Math.random() * 9999);
-simply.title('Your ID number is ' + socketId);
-localStorage.setItem('socketId', socketId);
+var session = function(socketId){
+  simply.title('Your ID number is ' + socketId);
+  localStorage.setItem('socketId', socketId);
 
-//Listen for accelerometer data.
-simply.on('accelTap', function(e) {
-  simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
-  var options = {
-    url: 'http://www.webpeb.com/accelerometer',
-    method: 'post',
-    data: {'direction': e.direction, 'axis': e.axis, 'pebID': socketId}
-  };
+  //Listen for accelerometer data.
+  simply.on('accelTap', function(e) {
+    simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+    var options = {
+      url: 'http://www.webpeb.com/accelerometer',
+      method: 'post',
+      data: {'direction': e.direction, 'axis': e.axis, 'pebID': socketId}
+    };
 
-  ajax(options, function(data){
-    simply.vibe('short');
+    ajax(options, function(data){
+      simply.vibe('short');
+    });
   });
-});
 
-//Listen for button clicks.
-simply.on('singleClick', function(e) {
-  simply.subtitle('You pressed the ' + e.button + ' button!');
-  var options = {
-    url: 'http://www.webpeb.com/button',
-    method: 'post',
-    data: {'button': e.button, 'pebID': socketId}
-  };
+  //Listen for button clicks.
+  simply.on('singleClick', function(e) {
+    simply.subtitle('You pressed the ' + e.button + ' button!');
+    var options = {
+      url: 'http://www.webpeb.com/button',
+      method: 'post',
+      data: {'button': e.button, 'pebID': socketId}
+    };
 
-  ajax(options, function(data){
-    simply.vibe('short');
+    ajax(options, function(data){
+      simply.vibe('short');
+    });
   });
+}
+
+ajax({'url': '107.170.250.170/initiateSession'}, function(data){
+  session(data.id);
 });
