70c5efafdca71cea2b6357e40e7870c2ad366c73
Sam Agnew <sagnew@eden.rutgers.edu>
2014-01-26 13:34:18 -0500

    Fixed spacing because someone mixed tabs and spaces and it was freaking me out

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -5,72 +5,72 @@ var minTab = 0;
 var maxTab = 0;
 var lastTabs = [];
 chrome.tabs.query({},function(tabs){
-    maxTab = tabs.length;
-    console.log(maxTab);
+  maxTab = tabs.length;
+  console.log(maxTab);
 });
 var currentId = 200;
 
 chrome.tabs.query({active: true}, function(tabs){
-    currentTab = tabs[0].index;
-    console.log(currentTab);
+  currentTab = tabs[0].index;
+  console.log(currentTab);
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-    currentId = message.id;
-    console.log("received content message");
-    console.log(message);
+  currentId = message.id;
+  console.log("received content message");
+  console.log(message);
 });
 
 setInterval(function(){
-$.get('http://107.170.250.170/getAction', {id: currentId}, function(data){
-	console.log(data);
+  $.get('http://107.170.250.170/getAction', {id: currentId}, function(data){
+    console.log(data);
 
-	if(data.action == "tabRight"){
-		currentTab++;
-		if(currentTab > maxTab)
-			currentTab = 0;
+    if(data.action == "tabRight"){
+      currentTab++;
+      if(currentTab > maxTab)
+    currentTab = 0;
 
-		chrome.tabs.highlight({tabs: currentTab}, function(window){
-			console.log(window);
-		});
-	}else if(data.action == "tabLeft"){
-		currentTab--;
-		if(currentTab < minTab)
-			currentTab = maxTab;
+  chrome.tabs.highlight({tabs: currentTab}, function(window){
+    console.log(window);
+  });
+    }else if(data.action == "tabLeft"){
+      currentTab--;
+      if(currentTab < minTab)
+    currentTab = maxTab;
 
-		chrome.tabs.highlight({tabs: currentTab}, function(window){
-			console.log(window);
-		});
-	}else if(data.action == "closeTab"){
-		chrome.tabs.query({active: true}, function(tabs){
-			lastTabs.push(tabs[0].url);
-			maxTab--;
-	
-			if(currentTab === maxTab)
-				currentTab--;
+  chrome.tabs.highlight({tabs: currentTab}, function(window){
+    console.log(window);
+  });
+    }else if(data.action == "closeTab"){
+      chrome.tabs.query({active: true}, function(tabs){
+        lastTabs.push(tabs[0].url);
+        maxTab--;
 
-			chrome.tabs.remove(tabs[0].id, function(){
-				console.log('' + lastTabs[lastTabs.length-1] + " removed");
-			});
-		});		
-        }else if(data.action == "reopenTab"){
-		
-	}else if(data.action == "scrollDown"){
-		chrome.tabs.query({active: true}, function(tabs){
-			console.log(tabs[0]);
-			chrome.tabs.executeScript(tabs[0].id, {code: 'window.scrollBy(0, 50);'}, function(results){
-				console.log(results);
-			});
-		});
-	}else if(data.action == "scrollUp"){
-		chrome.tabs.query({active: true}, function(tabs){
-			console.log(tabs[0]);
-			chrome.tabs.executeScript(tabs[0].id, {code: 'window.scrollBy(0, -50);'}, function(results){
-				console.log(results);
-			});
-		});
-	}else{
-	//	console.log("doing nothing");
-	}
+        if(currentTab === maxTab)
+        currentTab--;
+
+      chrome.tabs.remove(tabs[0].id, function(){
+        console.log('' + lastTabs[lastTabs.length-1] + " removed");
+      });
+      });
+    }else if(data.action == "reopenTab"){
+
+    }else if(data.action == "scrollDown"){
+      chrome.tabs.query({active: true}, function(tabs){
+        console.log(tabs[0]);
+        chrome.tabs.executeScript(tabs[0].id, {code: 'window.scrollBy(0, 50);'}, function(results){
+          console.log(results);
+        });
+      });
+    }else if(data.action == "scrollUp"){
+      chrome.tabs.query({active: true}, function(tabs){
+        console.log(tabs[0]);
+        chrome.tabs.executeScript(tabs[0].id, {code: 'window.scrollBy(0, -50);'}, function(results){
+          console.log(results);
+        });
+      });
+    }else{
+      //	console.log("doing nothing");
+    }
 });
-}, 1000);
+}, 250);
