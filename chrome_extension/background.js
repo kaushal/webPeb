923798b178d7535f38918bd2213b9c72fe32722d
Eric Song <regonics@gmail.com>
2014-01-26 02:17:28 -0500

    updated chrome extension to get prototype working

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,16 +1,46 @@
 console.log("hi");
 
 var currentTab = 0;
+var minTab = 0;
+var maxTab = 3;
+
+chrome.tabs.query({active: true}, function(tabs){
+	currentTab = tabs[0].index;
+	console.log(currentTab);
+});
+
+var currentId = 200;
 
 setInterval(function(){
-$.get('http://localhost:8000/getAction', function(data){
-	if(data.action == "change tabs"){
+$.get('http://107.170.250.170/getAction', {id: currentId}, function(data){
+	console.log(data);
+
+	if(data.action == "tabRight"){
+		currentTab++;
+		if(currentTab > maxTab)
+			currentTab = 0;
+
 		chrome.tabs.highlight({tabs: currentTab}, function(window){
 			console.log(window);	
-			currentTab++;
 		});		
-	}else if(data.action == "scroll down"){
-	        window.scrollBy(0,50);	
+	}else if(data.action == "tabLeft"){
+		currentTab--;
+		if(currentTab < minTab)
+			currentTab = maxTab;
+
+		chrome.tabs.highlight({tabs: currentTab}, function(window){
+			console.log(window);	
+		});		
+	}else if(data.action == "closeTab"){
+
+	}else if(data.action == "reopenTab"){
+
+	}else if(data.action == "scrollDown"){
+		window.scrollBy(0,50);	
+	}else if(data.action == "scrollUp"){
+		window.scrollBy(0,50);	
+	}else{
+	//	console.log("doing nothing");
 	}
 });
 }, 1000);
